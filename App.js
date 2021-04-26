757622f29105efb74042719e13cda955754a2e60
NEHAISRANI <nehai18@navgurukul.org>
2021-04-26 15:42:30 +0530

    added third Task

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+// import {View, Input, Text, Button, TextInput} from 'react-native';
+import Navigator from './source/routes/homeStack'
+
+export default function App(){
+    return(
+        <Navigator/>
+    )
+}
